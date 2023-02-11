734311632865006512aa926af5ca52428c492523
pchiragiev <you@example.com>
2023-02-10 19:30:49 -0600

    pokemon

--- Week1/controller3.js
+++ Week1/controller3.js
@@ -1,21 +1,31 @@
 /* Created By: Pavel Chiragiev */
 
-function addItem () {
-    let item = document.getElementById("inputHobby").value;
-    let addedItem = document.createElement("li");
-    addedItem.innerHTML = item;
-    document.getElementById('hobbies').appendChild(addedItem);
+function apiAlert () {
+    fetch('https://pokeapi.co/api/v2/pokemon/')
+        .then(response => response.text())
+        .then(data => alert(data));
 }
 
-function printList () {
-    let list = document.getElementById("hobbies").getElementsByTagName('li');
-    let message = "";
-    for (let i = 0; i < list.length; ++i) {
-        message += ` ${list[i].innerHTML}`;
-    }
-    console.log(message);
-    alert(message);
-}
+document.getElementById("apiAlert").addEventListener("click", apiAlert);
 
-document.getElementById("add").addEventListener("click", addItem);
-document.getElementById("display").addEventListener("click", printList);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// function addItem () {
+//     let item = document.getElementById("inputHobby").value;
+//     let addedItem = document.createElement("li");
+//     addedItem.innerHTML = item;
+//     document.getElementById('hobbies').appendChild(addedItem);
+// }
+
+// function printList () {
+//     let list = document.getElementById("hobbies").getElementsByTagName('li');
+//     let message = "";
+//     for (let i = 0; i < list.length; ++i) {
+//         message += ` ${list[i].innerHTML}`;
+//     }
+//     console.log(message);
+//     alert(message);
+// }
+
+// document.getElementById("add").addEventListener("click", addItem);
+// document.getElementById("display").addEventListener("click", printList);
